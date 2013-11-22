7d67cabf7e5c6c30f8abbb5398a4811928610e29
Tejun Heo <tj@kernel.org>
2013-11-22 18:20:44 -0500

    cgroup: unexport cgroup_css() and remove __file_cft()
    Now that cgroup_event is made memcg specific, the temporarily exported
    functions are no longer necessary.  Unexport cgroup_css() and remove
    __file_cft() which doesn't have any user left.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Li Zefan <lizefan@huawei.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Change-Id: Idc5e38c241abbfd2bfa195057f426337625e0622

--- include/linux/cgroup.h
+++ include/linux/cgroup.h
@@ -869,11 +869,6 @@ int cgroup_transfer_tasks(struct cgroup *to, struct cgroup *from);
 struct cgroup_subsys_state *css_from_dir(struct dentry *dentry,
 					 struct cgroup_subsys *ss);
 
-/* XXX: temporary */
-struct cgroup_subsys_state *cgroup_css(struct cgroup *cgrp,
-				       struct cgroup_subsys *ss);
-struct cftype *__file_cft(struct file *file);
-
 #else /* !CONFIG_CGROUPS */
 
 struct cgroup_subsys_state;
