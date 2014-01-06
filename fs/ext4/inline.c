9e55500a12b22a4f9171d72a759e193554f3e9f3
Jan Kara <jack@suse.cz>
2014-01-06 14:02:23 -0500

    ext4: retry allocation when inline->extent conversion failed
    Similarly as other ->write_begin functions in ext4, also
    ext4_da_write_inline_data_begin() should retry allocation if the
    conversion failed because of ENOSPC. This avoids returning ENOSPC
    prematurely because of uncommitted block deletions.
    
    Change-Id: I0a9c45eb442f366e859ad28760254b5e42c0660e
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -881,6 +881,7 @@ retry_journal:
 							    flags,
 							    fsdata);
 		ext4_journal_stop(handle);
+		handle = NULL;
 		if (ret == -ENOSPC &&
 		    ext4_should_retry_alloc(inode->i_sb, &retries))
 			goto retry_journal;
