2c2f56ca3e5f2638bd35ddf20cd42f1bbcd81832
Tejun Heo <tj@kernel.org>
2014-05-14 19:33:07 -0400

    cgroup: disallow debug controller on the default hierarchy
    The debug controller, as its name suggests, exposes cgroup core
    internals to userland to aid debugging.  Unfortunately, except for the
    name, there's no provision to prevent its usage in production
    configurations and the controller is widely enabled and mounted
    leaking internal details to userland.  Like most other debug
    information, the information exposed by debug isn't interesting even
    for debugging itself once the related parts are working reliably.
    
    This controller has no reason for existing.  This patch implements
    cgrp_dfl_root_inhibit_ss_mask which can suppress specific subsystems
    on the default hierarchy and adds the debug subsystem to it so that it
    can be gradually deprecated as usages move towards the unified
    hierarchy.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Change-Id: I6361d60962c02014cee06960717d3552e5ae46bb

--- include/linux/cgroup_subsys.h
+++ include/linux/cgroup_subsys.h
@@ -6,10 +6,6 @@
 SUBSYS(cpuset)
 #endif
 
-#if IS_ENABLED(CONFIG_CGROUP_DEBUG)
-SUBSYS(debug)
-#endif
-
 #if IS_ENABLED(CONFIG_CGROUP_SCHED)
 SUBSYS(cpu)
 #endif
@@ -61,3 +57,13 @@ SUBSYS(bcache)
 
 
 /* */
+
+/*
+ * The following subsystems are not supported on the default hierarchy.
+ */
+#if IS_ENABLED(CONFIG_CGROUP_DEBUG)
+SUBSYS(debug)
+#endif
+/*
+ * DO NOT ADD ANY SUBSYSTEM WITHOUT EXPLICIT ACKS FROM CGROUP MAINTAINERS.
+ */
