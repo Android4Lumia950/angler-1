8ab0267879481942067d0717e43141f78a24f854
Dmitry Torokhov <dtor@chromium.org>
2016-10-06 15:53:38 -0700

    CHROMIUM: remove Android's cgroup generic permissions checks
    The implementation is utterly broken, resulting in all processes being
    allows to move tasks between sets (as long as they have access to the
    "tasks" attribute), and upstream is heading towards checking only
    capability anyway, so let's get rid of this code.
    
    BUG=b:31790445,chromium:647994
    TEST=Boot android container, examine logcat
    
    Change-Id: I2f780a5992c34e52a8f2d0b3557fc9d490da2779
    Signed-off-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/394967
    Reviewed-by: Ricky Zhou <rickyz@chromium.org>
    Reviewed-by: John Stultz <john.stultz@linaro.org>
    (cherry picked from commit 6895149f8bf0719aa70487e285fa6a8ad3d2692d)
    Reviewed-on: https://chromium-review.googlesource.com/399858
    Reviewed-by: Guenter Roeck <groeck@chromium.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9781,23 +9781,6 @@ static void cpu_cgroup_css_released(struct cgroup_subsys_state *css)
 	sched_offline_group(tg);
 }
 
-static int
-cpu_cgroup_allow_attach(struct cgroup *cgrp, struct cgroup_taskset *tset)
-{
-	const struct cred *cred = current_cred(), *tcred;
-	struct task_struct *task;
-
-	cgroup_taskset_for_each(task, cgrp, tset) {
-		tcred = __task_cred(task);
-
-		if ((current != task) && !capable(CAP_SYS_NICE) &&
-		    cred->euid != tcred->uid && cred->euid != tcred->suid)
-			return -EACCES;
-	}
-
-	return 0;
-}
-
 static int cpu_cgroup_can_attach(struct cgroup *cgrp,
 				 struct cgroup_taskset *tset)
 {
@@ -10222,7 +10205,6 @@ struct cgroup_subsys cpu_cgrp_subsys = {
 	.fork		= cpu_cgroup_fork,
 	.can_attach	= cpu_cgroup_can_attach,
 	.attach		= cpu_cgroup_attach,
-	.allow_attach	= cpu_cgroup_allow_attach,
 	.exit		= cpu_cgroup_exit,
 	.subsys_id	= cpu_cgroup_subsys_id,
 	.base_cftypes	= cpu_files,
