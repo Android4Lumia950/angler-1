83f2c1ed7373f7e489eb76e37618f8947e9a7389
J0SH1X <aljoshua.hell@gmail.com>
2025-02-25 15:57:45 +0100

    Revert "BACKPORT: fs: call rename2 if exists"
    This reverts commit 5995f549b50d7c3a1457633d79b3d357a83af0e8.

--- fs/namei.c
+++ fs/namei.c
@@ -3856,11 +3856,10 @@ static int vfs_rename_dir(struct vfsmount *mnt,
 
 	if (target)
 		shrink_dcache_parent(new_dentry);
-	if (!old_dir->i_op->rename2) {
+	if (!flags) {
 		error = old_dir->i_op->rename(old_dir, old_dentry,
 					      new_dir, new_dentry);
 	} else {
-		WARN_ON(old_dir->i_op->rename != NULL);
 		error = old_dir->i_op->rename2(old_dir, old_dentry,
 					       new_dir, new_dentry, flags);
 	}
@@ -3900,11 +3899,10 @@ static int vfs_rename_other(struct inode *old_dir, struct dentry *old_dentry,
 	if (d_mountpoint(old_dentry)||d_mountpoint(new_dentry))
 		goto out;
 
-	if (!old_dir->i_op->rename2) {
+	if (!flags) {
 		error = old_dir->i_op->rename(old_dir, old_dentry,
 					      new_dir, new_dentry);
 	} else {
-		WARN_ON(old_dir->i_op->rename != NULL);
 		error = old_dir->i_op->rename2(old_dir, old_dentry,
 					       new_dir, new_dentry, flags);
 	}
@@ -3945,7 +3943,7 @@ int vfs_rename2(struct vfsmount *mnt,
 	if (error)
 		return error;
 
-	if (!old_dir->i_op->rename && !old_dir->i_op->rename2)
+	if (!old_dir->i_op->rename)
 		return -EPERM;
 
 	if (flags && !old_dir->i_op->rename2)
